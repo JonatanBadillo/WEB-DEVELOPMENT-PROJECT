fb330a567acc8560b5d64bb747262be93f0c1ab6
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-10-11 08:08:31 -0600

    verifying not to repeat mail

--- php/registro_usuario_br.php
+++ php/registro_usuario_br.php
@@ -6,13 +6,27 @@
     $correo = $_POST['correo'];
     $usuario = $_POST['usuario'];
     $contrasena = $_POST['contrasena'];
-    $contrasena = hash('sha512',$contrasena); // contrasena encriptacion
+    
+    // $contrasena = hash('sha512',$contrasena); // contrasena encriptacion
 
+    
     $query = "INSERT INTO usuarios(nombre_completo,correo,usuario,contrasena)
               VALUES('$nombre_completo','$correo','$usuario','$contrasena')";
-
+    
+    
+    // Verificar que el correo no se repita en la BD
+    $verificar_correo = mysqli_query($conexion,"SELECT * FROM usuarios where correo='$correo'");
+    if(mysqli_num_rows($verificar_correo) > 0 ){ // si ecnuentra un dato repetido
+        echo'
+            <script>
+                alert("Este correo ya esta registrado, intenta con otro diferente");
+                window.location = "../index.php";
+            </script>
+        ';
+        exit();
+    }
+    
     $ejecutar = mysqli_query($conexion, $query);
-
     if($ejecutar){
         echo '
             <script>
