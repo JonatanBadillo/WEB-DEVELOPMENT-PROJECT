522b23aca65179e7d046e9a96bcc5f6d2345eb2b
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-10-10 20:20:15 -0600

    proving test php

---
+++ PRUEBA/php/registro_usuario_br.php
@@ -0,0 +1,14 @@
+<?php
+
+    include 'conexion_be.php';
+
+    $nombre_completo = $_POST['nombre_completo'];
+    $correo = $_POST['correo'];
+    $usuario = $_POST['usuario'];
+    $contrasena = $_POST['contrasena'];
+
+    $query = "INSERT INTO usuarios(nombre_completo,correo,usuario,contrasena)
+              VALUES('$nombre_completo','$correo','$usuario','$contrasena')";
+
+    $ejecutar = mysqli_query($conexion, $query);
+?>
