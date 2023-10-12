021188d890532517a40a8ffc70dca0c814968c09
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-10-11 18:22:36 -0600

    loading data to the DATA BASE

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,12 +1,12 @@
 <?php
     
     $conexion = mysqli_connect("localhost","root","","login_register_db"); // Estableciendo conexion con la BD
-    
+    /*
     if($conexion){
         echo 'Conectado exitosamente a la Base de datos';
     }else{
         echo 'No se ha podido conectar a la base de datos';
     }
-    
+    */
     
 ?>
---
+++ php/registro_usuario_be.php
@@ -0,0 +1,58 @@
+<?php
+
+    include 'conexion_be.php';
+
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $correo = $_POST['correo'];
+    $usuario = $_POST['usuario'];
+    $contrasena = $_POST['contrasena'];
+    
+    // $contrasena = hash('sha512',$contrasena); // contrasena encriptacion
+
+    
+    $query = "INSERT INTO usuarios(nombre,apellido,correo,usuario,contrasena)
+              VALUES('$nombre','$apellido' ,'$correo','$usuario','$contrasena')";
+    
+    
+    // Verificar que el correo no se repita en la BD
+    $verificar_correo = mysqli_query($conexion,"SELECT * FROM usuarios where correo='$correo'");
+    if(mysqli_num_rows($verificar_correo) > 0 ){ // si encuentra un dato repetido
+        echo'
+            <script>
+                alert("Este correo ya esta registrado, intenta con otro diferente");
+                window.location = "../index.php";
+            </script>
+        ';
+        exit();
+    }
+    // Verificar que el user no se repita en la BD
+    $verificar_usuario = mysqli_query($conexion,"SELECT * FROM usuarios where usuario='$usuario'");
+    if(mysqli_num_rows($verificar_usuario) > 0 ){ // si encuentra un dato repetido
+        echo'
+            <script>
+                alert("Este usuario ya esta registrado, intenta con otro diferente");
+                window.location = "../index.php";
+            </script>
+        ';
+        exit();
+    }
+    $ejecutar = mysqli_query($conexion, $query);
+    if($ejecutar){
+        echo '
+            <script>
+                alert("Usuario almacenado exitosamente");
+                window.location = "../index.php"
+            </script>
+        ';
+    }else{
+        echo '
+        <script>
+            alert("Intentalo de nuevo, usuario no almacenado");
+            window.location = "../index.php"
+        </script>
+    '; 
+    }
+
+    mysqli_close($conexion);
+?>
