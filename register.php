33b6bf9f2ad9ecd28249e14ca56788c84d165b44
Javier <javierdolorestolentino@gmail.com>
2023-10-11 21:54:30 -0600

    Register validando correctamente los datos y dando de alta usuarios

--- register.php
+++ register.php
@@ -11,7 +11,55 @@
     <section>
         <div class="contenedor2">
             <div class="formulario">
-                <form action="php/registro_usuario_be.php" method="POST" class="formulario__register">
+                <form action="register.php" method="POST" class="formulario__register">
+                    <?php
+                        if (isset($_POST['nombre'])) {
+                            $nombre = $_POST['nombre'];
+                            $apellido = $_POST['apellido'];
+                            $correo = $_POST['correo'];
+                            $usuario = $_POST['usuario'];
+                            $contrasena = $_POST['contrasena'];
+
+                            $campos = array();
+
+                            if ($nombre == "") {
+                                array_push($campos, "El campo nombre no puede estar vacío.");
+                            } elseif (strlen($nombre) < 3 || strlen($nombre) > 15) {
+                                array_push($campos, "El campo nombre debe tener entre 3 y 15 caracteres.");
+                            }
+
+                            if ($apellido == "") {
+                                array_push($campos, "El campo apellido no puede estar vacío.");
+                            } elseif (strlen($apellido) < 5 || strlen($apellido) > 20) {
+                                array_push($campos, "El campo apellido debe tener entre 5 y 20 caracteres.");
+                            }
+
+                            if ($correo == "" || strpos($correo, "@") === false) {
+                                array_push($campos, "Ingrese un correo electrónico válido.");
+                            }
+
+                            if ($usuario == "") {
+                                array_push($campos, "El campo usuario no puede estar vacío.");
+                            } elseif (strlen($usuario) < 5 || strlen($usuario) > 15) {
+                                array_push($campos, "El campo usuario debe tener entre 5 y 15 caracteres.");
+                            }
+
+                            if ($contrasena == "" || strlen($contrasena) < 6 || strlen($contrasena) > 15) {
+                                array_push($campos, "El campo contraseña debe tener entre 6 y 15 caracteres.");
+                            }
+
+                            if (count($campos) > 0) {
+                                echo "<div class='error'>";
+                                for ($i = 0; $i < count($campos); $i++) {
+                                    echo "<li>" . $campos[$i] . "</li>";
+                                }
+                                echo "</div>";
+                            } else {
+                                header('Location: php/registro_usuario_be.php');
+                            }
+                        }
+                    ?>
+    
                     <h2>Registro</h2>
 
                     <div class="input-contenedor">
@@ -47,7 +95,7 @@
                     </div>
 
                     <div class="center">
-                        <input type="submit" value="Registrarse" class="button">
+                        <input type="submit" value="Registrarse" class="button" >
                     </div>
                 </form>
 
