0a58756c7110689b8eb44c5da866020c194401d7
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-10-11 18:06:45 -0600

    creating index and register PHP

--- index.php
+++ index.php
@@ -5,25 +5,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="/css/estilos2.css">
+    <link rel="stylesheet" href="assets/estilos__login.css">
     <script src="https://kit.fontawesome.com/8201687cad.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <section>
-        <div class="contenedor">
+        <div class="contenedor__todo">
             <div class="formulario">
-                <form action="#">
+                <form action="php/login_usuario_be.php" method="POST" class="formulario__login">
                     <h2>Iniciar Sesi&oacute;n</h2>
 
                     <div class="input-contenedor">
                         <i class="fa-solid fa-user"></i>
-                        <input type="username" required>
+                        <input type="text" name="usuario" required>
                         <label for="#">Usuario</label>
                     </div>
 
                     <div class="input-contenedor">
                         <i class="fa-solid fa-lock"></i>
-                        <input type="password" required>
+                        <input type="password" name="contrasena" required>
                         <label for="#">Contraseña</label>
                     </div>
 
@@ -32,7 +32,7 @@
                 <div>
                     <button>Acceder</button>
                     <div class="registrar">
-                        <p>No tengo Cuenta <a href="/html/login.html">Crear Cuenta</a></p>
+                        <p>No tengo Cuenta <a href="register.php">Crear Cuenta</a></p>
                     </div>
                 </div>
 
--- register.php
+++ register.php
@@ -4,44 +4,44 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="/css/estilos2.css">
+    <link rel="stylesheet" href="assets/estilos__login.css">
     <script src="https://kit.fontawesome.com/8201687cad.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <section>
         <div class="contenedor2">
             <div class="formulario">
-                <form action="/php/validation.php" method="post" >
+                <form action="php/registro_usuario_br.php" method="POST" class="formulario__register">
                     <h2>Registro</h2>
 
                     <div class="input-contenedor">
                         <i class="fa-solid fa-circle-user"></i>
-                        <input type="name" name="name" required>
+                        <input type="text" name="nombre" required>
                         <label for="#">Nombre</label>
                         
                     </div>
 
                     <div class="input-contenedor">
                         <i class="fa-solid fa-circle-user"></i>
-                        <input type="lastname" name="lastname" required>
+                        <input type="text" name="apellido" required>
                         <label for="#">Apellido</label>
                     </div>
 
                     <div class="input-contenedor">
                         <i class="fa-solid fa-envelope"></i>
-                        <input type="email" name="email" required>
-                        <label for="#">Correo</label>
+                        <input type="text" name="correo" required>
+                        <label for="#">Correo Electronico</label>
                     </div>
 
                     <div class="input-contenedor">
                         <i class="fa-solid fa-user"></i>
-                        <input type="username" name="username" required>
+                        <input type="text" name="usuario" required>
                         <label for="#">Username</label>
                     </div>
 
                     <div class="input-contenedor">
                         <i class="fa-solid fa-lock"></i>
-                        <input type="password" name="password" required>
+                        <input type="password" name="contrasena" required>
                         <label for="#">Contraseña</label>
                         
                     </div>
@@ -53,7 +53,7 @@
 
                 <div>
                     <div class="registrar">
-                        <p>Tengo una Cuenta <a href="/html/signin.html">Ingresar</a></p>
+                        <p>Tengo una Cuenta <a href="index.php">Ingresar</a></p>
                     </div>
                 </div>
 
