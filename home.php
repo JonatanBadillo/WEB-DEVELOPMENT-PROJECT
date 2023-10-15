5c4b93ecb003d6eef717db3e2ed2d421e8f9e831
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-10-14 18:29:12 -0600

    script menu added

--- home.php
+++ home.php
@@ -22,6 +22,7 @@ if (!isset($_SESSION['usuario'])) { // Si sesion no existe
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>+Futbol</title>
+    <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="assets/css/style-home.css">
 </head>
 
