824a66e1c29b473c07a9f099bc98746027a43f0a
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-10-15 16:17:34 -0600

    few corrections

--- js/script-home.js
+++ js/script-home.js
@@ -8,8 +8,8 @@ function mostrar_menu(){
 
 window.onscroll = function () {
     var posicion = window.pageYOffset || document.documentElement.scrollTop;
-    var elemento1 = document.getElementById("icon_heart");
-    var elemento2 = document.getElementById("icon_fire");
+    var elemento1 = document.getElementById("icon_ball");
+    var elemento2 = document.getElementById("icon_player");
     elemento1.style.bottom = posicion * 0.1 + "px";
     elemento2.style.top = posicion * 0.1 + "px";
 }
