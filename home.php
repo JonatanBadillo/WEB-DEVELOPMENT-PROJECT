f6553b9e9613fd760c88a0667c06185d0a44a07a
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-10-16 07:55:07 -0600

    cards added

--- home.php
+++ home.php
@@ -47,6 +47,55 @@ if (!isset($_SESSION['usuario'])) { // Si sesion no existe
             margin-bottom: 5px;
             margin-top: 5px;
         }
+
+        /* TARJETA JUGADORES */
+        .container {
+            width: 100%;
+            max-width: 1200px;
+            height: 430px;
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            margin: auto;
+        }
+
+        .container .card {
+            width: 330px;
+            height: 430px;
+            border-radius: 8px;
+            box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
+            overflow: hidden;
+            margin: 20px;
+            text-align: center;
+            transition: all 0.25s;
+        }
+
+        .container .card:hover {
+            transform: translateY(-15px);
+            box-shadow: 0 12px 16px rgba(0, 0, 0, 0.2);
+        }
+
+        .container .card img {
+            width: 330px;
+            height: 220px;
+        }
+
+
+        .container .card h4 {
+            font-weight: 600;
+        }
+
+        .container .card p {
+            padding: 0 1rem;
+            font-size: 16px;
+            font-weight: 300;
+        }
+
+        .container .card a {
+            font-weight: 500;
+            text-decoration: none;
+            color: #3498db;
+        }
     </style>
 </head>
 
@@ -118,6 +167,36 @@ if (!isset($_SESSION['usuario'])) { // Si sesion no existe
             </div>
 
         </div>
+        <br>
+        <br>
+        <br>
+        <br>
+        <h1 class="title">Las mayores promesas del futbol</h1>
+
+        <div class="container">
+
+            <div class="card">
+                <img src="assets/images/player1.jpg">
+                <h4>Gavi</h4>
+                <p>Gavi es un jugador muy intenso y con mucho carácter, lo que le hace ser una pieza importante para el equipo en fase defensiva.</p>
+                <a href="#">Leer más</a>
+            </div>
+
+            <div class="card">
+                <img src="assets/images/player2.jpg">
+                <h4>Jude Bellingham</h4>
+                <p>Jugador muy versátil, capaz de jugar en varias posiciones del centro del campo. Tiene una gran calidad técnica, visión de juego y capacidad de asociación.</p>
+                <a href="#">Leer más</a>
+            </div>
+
+            <div class="card">
+                <img src="assets/images/player3.jpg">
+                <h4>Florian Wirtz</h4>
+                <p>Tiene una gran visión de juego, capacidad de pase y regate. También es un jugador muy habilidoso y con buen disparo.</p>
+                <a href="#">Leer más</a>
+            </div>
+
+        </div>
 
     </main>
 
@@ -125,10 +204,10 @@ if (!isset($_SESSION['usuario'])) { // Si sesion no existe
 
     <footer>
         <p>Todos los derechos reservados - 2023</p>
-        
+
         <div>
             <a href="https://www.youtube.com/" target="_blank"><i class="fa-brands fa-youtube"></i></a>
-            <a href="https://www.facebook.com/" target="_blank"><i class="fa-brands fa-facebook"></i></a>                    
+            <a href="https://www.facebook.com/" target="_blank"><i class="fa-brands fa-facebook"></i></a>
         </div>
 
     </footer>
