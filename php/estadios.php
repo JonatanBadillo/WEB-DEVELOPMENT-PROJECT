edc619ffad72ea992021971f4a9358efc3fc9ded
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2023-11-02 12:19:13 -0600

    template for stadiums

--- php/estadios.php
+++ php/estadios.php
@@ -23,6 +23,12 @@ if (!isset($_SESSION['usuario'])) { // Si sesion no existe
     <title>ESTADIOS</title>
     <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../assets/css/style-home.css">
+    <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../assets/css/style-home.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <style>
         /* Estilos CSS para la sección de estadios */
         body {
@@ -33,45 +39,87 @@ if (!isset($_SESSION['usuario'])) { // Si sesion no existe
             color: #333;
         }
 
-        .container {
-            max-width: 1200px;
-            margin: 0 auto;
-            padding: 20px;
-        }
+        .stadium-section {
+    text-align: center;
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 8px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    margin: 20px 0;
+    max-width: 100%;
+}
 
-        h1 {
-            font-size: 36px;
-            text-align: center;
-            margin-bottom: 20px;
-        }
+.stadium-section h2 {
+    font-size: 24px;
+}
 
-        .estadio {
-            border: 1px solid #eee;
-            border-radius: 10px;
-            padding: 20px;
-            margin: 20px 0;
-            background-color: #fff;
-            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-        }
+.stadium-section h3 {
+    font-size: 20px;
+}
 
-        .estadio img {
-            max-width: 100%;
-            height: auto;
-            border-radius: 10px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-        }
+.carousel-container {
+    position: relative;
+    max-width: 100%;
+}
 
-        h2 {
-            font-size: 28px;
-            margin: 10px 0;
-            color: #000;
-        }
+.carousel {
+    display: flex;
+    overflow: hidden;
+}
 
-        p {
-            font-size: 18px;
-            line-height: 1.6;
-            color: #444;
-        }
+.carousel-item {
+    flex: 0 0 100%;
+    text-align: center;
+    padding: 10px;
+}
+
+.carousel-item img {
+    max-width: 100%;
+    height: auto;
+    border: 2px solid #333;
+    border-radius: 8px;
+}
+
+/* Estilos para los botones del carrusel */
+.prev-button,
+.next-button {
+    background: #333;
+    color: #fff;
+    padding: 8px 16px;
+    border: none;
+    cursor: pointer;
+}
+
+/* Estilos para la descripción de los jugadores */
+.carousel-caption {
+    background-color: rgba(0, 0, 0, 0.8);
+    border-radius: 5px;
+    padding: 10px;
+}
+
+
+/* Estilos para las imágenes en pantallas grandes */
+.carousel-item img {
+    max-width: 100%;
+    height: auto;
+    border: 2px solid #333;
+    border-radius: 8px;
+    width: 1000px; /* Tamaño fijo para pantallas grandes */
+
+}
+
+/* Media query para ajustar el tamaño en pantallas más pequeñas */
+@media (max-width: 768px) {
+    .carousel-item img {
+        width: 100%; /* Ajusta el tamaño al ancho de la pantalla en pantallas más pequeñas */
+    }
+}
+
+    /* Estilo para los carruseles */
+    .carousel-container {
+        max-width: 80%;
+        margin: 0 auto;
+    }
     </style>
 
 </head>
@@ -133,7 +181,7 @@ if (!isset($_SESSION['usuario'])) { // Si sesion no existe
                         <!-- INSERTAR LOS ESTADIOS MAS EMBLEMATICOS DEL MUNDO--->
                         <li><a href="estadios.php">Estadios</a></li>
                         <!-- INSERTAR LOS MEJORES JUGADORES DE LA HISTORIA--->
-                        <li><a href="goats.php">GOATS</a></li>
+                        <li><a href="stadium.php">stadium</a></li>
                     </ul>
                 </nav>
 
@@ -149,42 +197,217 @@ if (!isset($_SESSION['usuario'])) { // Si sesion no existe
 
     </header>
     <main>
-    <div class="container">
-            <h1>Estadios Emblemáticos del Mundo</h1>
-            <!-- Estadio 1 -->
-            <div class="estadio">
-                <img src="../assets/images/Camp-Nou-1.jpg" alt="Estadio 1">
-                <center><h2> Estadio Camp Nou</h2></center>
-                <p>El Camp Nou es un estadio icónico del fútbol mundial. Ubicado en Barcelona, España, es el estadio del Fútbol Club Barcelona y tiene una capacidad de 99.354 espectadores. Fue inaugurado en 1957 y ha sido sede de numerosos partidos importantes, como la final de la Copa Mundial de la FIFA 1982 y la final de la Liga de Campeones de la UEFA 1992.</p>
-            </div>
-            <!-- Estadio 2 -->
-            <div class="estadio">
-                <img src="../assets/images/Maracana.jpg" alt="Estadio 2">
-                <center><h2>Estadio Maracaná</h2></center>
-                <p>El Estadio Maracaná es el estadio más grande de América del Sur y uno de los más grandes del mundo. Se encuentra en Río de Janeiro, Brasil, y es el hogar de la selección brasileña de fútbol. El estadio fue inaugurado en 1950 y ha sido sede de dos finales de la Copa del Mundo, la final de la Copa América de 2014 y los Juegos Olímpicos de 2016.</p>
-            </div>
-            <!-- Estadio 3 -->
-            <div class="estadio">
-                <img src="../assets/images/Azteca.jpg" alt="Estadio 3">
-                <center><h2>Estadio Azteca</h2></center>
-                <p>El Estadio Azteca es el estadio más grande de México y el segundo más grande de América del Norte. Se encuentra en la Ciudad de México y es el hogar del Club América y de la selección mexicana de fútbol. El estadio fue inaugurado en 1966 y ha sido sede de dos finales de la Copa del Mundo, dos finales de la Copa América y los Juegos Olímpicos de 1968.</p>
-            </div>
-            <!-- Estadio 4 -->
-            <div class="estadio">
-                <img src="../assets/images/Bernabeu.jpg" alt="Estadio 4">
-                <center><h2>Estadio Santiago Bernabéu</h2></center>
-                <p>El Estadio Santiago Bernabéu es el estadio más grande de España y uno de los más grandes de Europa. Se encuentra en Madrid y es el hogar del Real Madrid. El estadio fue inaugurado en 1947 y ha sido sede de dos finales de la Copa del Mundo, tres finales de la Liga de Campeones de la UEFA y la final de la Copa de Europa de 1957.</p>
-            </div>
-            <!-- Estadio 5 -->
-            <div class="estadio">
-                <img src="../assets/images/Liverpool-Anfield.jpg" alt="Estadio 5">
-                <center><h2>Estadio Anfield</h2></center>
-                <p>Anfield es el estadio más grande de Inglaterra y el hogar del Liverpool. Se encuentra en Liverpool y fue inaugurado en 1884. Anfield es conocido por su atmósfera única y su importancia cultural. El estadio ha sido sede de numerosas finales de la FA Cup y de la final de la Liga de Campeones de la UEFA de 2005.
+    <main>
+    <center><h2>Los Mejores Jugadores de la Historia</h2></center>
+    <div class="stadium-section">
+    
+    <h3>Lionel Andrés Messi Cuccittini</h3>
+    <div id="player-carousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicador para las imagenes -->
+        <ol class="carousel-indicators">
+            <li data-target="#player-carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#player-carousel" data-slide-to="1"></li>
+            <li data-target="#player-carousel" data-slide-to="2"></li> 
+            <li data-target="#player-carousel" data-slide-to="3"></li>
+            <li data-target="#player-carousel" data-slide-to="4"></li>
+        </ol>
 
-Estos son solo algunos de los estadios más emblemáticos del mundo. Cada uno de estos estadios tiene su propia historia y su propio significado para los fanáticos del fútbol.</p>
+        <!-- Imágenes del carrusel -->
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="../assets/images/messi1.jpg" alt="Jugador 1 - Imagen 1">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/messi2.jpg" alt="Jugador 1 - Imagen 2">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/messi3.jpg" alt="Jugador 1 - Imagen 3">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/messi4.jpg" alt="Jugador 1 - Imagen 4">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/messi5.jpg" alt="Jugador 1 - Imagen 4">
             </div>
         </div>
 
+        <!-- Carrusel Caption -->
+        <div class="carousel-caption">
+            <p style="background-color: black; border-radius: 5px; opacity: 0.8;">Messi es un poeta del fútbol, un artista que pinta con el balón. Su juego es fluido y elegante, como un tango. Es imposible predecir sus movimientos, es impredecible. Es un genio, un talento único..</p>
+        </div>
+
+        <!-- Controles del carrusel -->
+        <a class="carousel-control-prev" href="#player-carousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+        </a>
+        <a class="carousel-control-next" href="#player-carousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Siguiente</span>
+        </a>
+    </div>
+</div>
+
+
+<div class="stadium-section">
+    <h3>Diego Armando Maradona Franco</h3>
+    <div id="player-carousel-2" class="carousel slide" data-ride="carousel">
+        <!-- Indicadores -->
+        <ol class="carousel-indicators">
+            <li data-target="#player-carousel-2" data-slide-to="0" class="active"></li>
+            <li data-target="#player-carousel-2" data-slide-to="1"></li>
+            <li data-target="#player-carousel-2" data-slide-to="2"></li> 
+            <li data-target="#player-carousel-2" data-slide-to="3"></li>
+            <li data-target="#player-carousel-2" data-slide-to="4"></li>
+        </ol>
+
+        <!-- Imágenes del carrusel para Jugador 2 -->
+        <div class="carousel-inner">
+        <div class="carousel-item active">
+                <img src="../assets/images/maradona1.jpg" alt="Jugador 2 - Imagen 1">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/maradona2.jpg" alt="Jugador 2 - Imagen 2">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/maradona3.jpg" alt="Jugador 2 - Imagen 3">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/maradona4.jpg" alt="Jugador 2 - Imagen 4">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/maradona5.jpg" alt="Jugador 2 - Imagen 4">
+            </div>
+
+        </div>
+
+        <!-- Carrusel Caption para Jugador 2 -->
+        <div class="carousel-caption">
+        <p style="background-color: black; border-radius: 5px; opacity: 0.8;">Maradona, un dios del fútbol,
+con el balón en los pies,
+era una obra de arte en movimiento,
+un poema de amor al juego.</p>
+        </div>
+
+        <!-- Controles del carrusel para Jugador 2 -->
+        <a class="carousel-control-prev" href="#player-carousel-2" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+        </a>
+        <a class="carousel-control-next" href="#player-carousel-2" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Siguiente</span>
+        </a>
+    </div>
+</div>
+
+<div class="stadium-section">
+    <h3>Cristiano Ronaldo dos Santos Aveiro</h3>
+    <div id="player-carousel-3" class="carousel slide" data-ride="carousel">
+        <!-- Indicadores -->
+        <ol class="carousel-indicators">
+            <li data-target="#player-carousel-3" data-slide-to="0" class="active"></li>
+            <li data-target="#player-carousel-3" data-slide-to="1"></li>
+            <li data-target="#player-carousel-3" data-slide-to="2"></li> 
+            <li data-target="#player-carousel-3" data-slide-to="3"></li>
+            <li data-target="#player-carousel-3" data-slide-to="4"></li>
+        </ol>
+
+        <!-- Imágenes del carrusel para Jugador 2 -->
+        <div class="carousel-inner">
+        <div class="carousel-item active">
+                <img src="../assets/images/cr71.jpg" alt="Jugador 3 - Imagen 1">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/cr72.jpg" alt="Jugador 3 - Imagen 2">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/cr73.jpg" alt="Jugador 3 - Imagen 3">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/cr74.jpg" alt="Jugador 3 - Imagen 4">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/cr75.jpg" alt="Jugador 3 - Imagen 4">
+            </div>
+            <!-- Agrega más imágenes para Jugador 2 según sea necesario -->
+        </div>
+
+        <!-- Carrusel Caption para Jugador 2 -->
+        <div class="carousel-caption">
+        <p style="background-color: black; border-radius: 5px; opacity: 0.8;">Cristiano Ronaldo.
+        Un halcón sobre el campo,
+un cazador de goles,
+con la mirada fija en el arco,
+y la determinación en su corazón..</p>
+        </div>
+
+        <!-- Controles del carrusel para Jugador 2 -->
+        <a class="carousel-control-prev" href="#player-carousel-3" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+        </a>
+        <a class="carousel-control-next" href="#player-carousel-3" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Siguiente</span>
+        </a>
+    </div>
+</div>
+
+<div class="stadium-section">
+    <h3>Edson Arantes do Nascimento</h3>
+    <div id="player-carousel-4" class="carousel slide" data-ride="carousel">
+        <!-- Indicadores -->
+        <ol class="carousel-indicators">
+            <li data-target="#player-carousel-4" data-slide-to="0" class="active"></li>
+            <li data-target="#player-carousel-4" data-slide-to="1"></li>
+            <li data-target="#player-carousel-4" data-slide-to="2"></li> 
+            <li data-target="#player-carousel-4" data-slide-to="3"></li>
+            <li data-target="#player-carousel-4" data-slide-to="4"></li>
+        </ol>
+
+        <!-- Imágenes del carrusel para Jugador 2 -->
+        <div class="carousel-inner">
+        <div class="carousel-item active">
+                <img src="../assets/images/pele2.jpg" alt="Jugador 4 - Imagen 1">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/pele1.jpg" alt="Jugador 4 - Imagen 2">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/pele3.jpg" alt="Jugador 4 - Imagen 3">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/pele4.jpg" alt="Jugador 4 - Imagen 4">
+            </div>
+            <div class="carousel-item">
+                <img src="../assets/images/pele5.jpg" alt="Jugador 4 - Imagen 4">
+            </div>
+            <!-- Agrega más imágenes para Jugador 2 según sea necesario -->
+        </div>
+
+        <!-- Carrusel Caption para Jugador 2 -->
+        <div class="carousel-caption">
+        <p style="background-color: black; border-radius: 5px; opacity: 0.8;">Pelé, el rey del fútbol,
+un dios de la cancha,
+con el balón en sus pies,
+era una obra de arte en movimiento.</p>
+        </div>
+
+        <!-- Controles del carrusel para Jugador 2 -->
+        <a class="carousel-control-prev" href="#player-carousel-4" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+        </a>
+        <a class="carousel-control-next" href="#player-carousel-4" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Siguiente</span>
+        </a>
+    </div>
+</div>
+
+
+</main>
 
     </main>
     <!--
